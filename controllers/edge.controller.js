bf35cc804fc77d2a66795201b6e678797d64eede
Théo CHELIM <theochelim@gmail.com>
2021-08-14 23:01:46 +0200

    Add get cpu and storage controller functions

--- controllers/edge.controller.js
+++ controllers/edge.controller.js
@@ -1,4 +1,5 @@
 const ping = require("ping");
+const child_process = require("child_process");
 
 module.exports.allEdges = (req, res) => {};
 module.exports.updateConfig = (req, res) => {};
@@ -9,22 +10,50 @@ module.exports.reboot = (req, res) => {};
 module.exports.stress = (req, res) => {};
 
 module.exports.getAvailability = async (req, res) => {
-  var host = "127.0.0.1";
-  if (req.params.id == 6) {
-    host = "172.16.100." + req.params.id;
+  if (req.params.id > 0) {
+    const host = "172.16.100." + req.params.id;
+    const cfg = {
+      timeout: 2,
+      extra: ["-c", "2"],
+    };
+    await ping.sys.probe(
+      host,
+      function (isAlive) {
+        res.status(200).json({ available: isAlive });
+      },
+      cfg
+    );
+  } else {
+    res.status(500).json("ID not valid");
   }
-  const cfg = {
-    timeout: 2,
-    extra: ["-i", "2"],
-  };
-  await ping.sys.probe(
-    host,
-    function (isAlive) {
-      res.status(200).json({ available: isAlive });
-    },
-    cfg
+};
+
+module.exports.getCpuUsage = (req, res) => {
+  child_process.exec(
+    "ssh " + edge + " vmstat 1 2 | awk 'NR==4 {print ($13+$14)}'",
+    (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return null;
+      }
+      if (stderr) {
+        return null;
+      }
+      return stdout.substring(0, 3);
+    }
   );
 };
 
-module.exports.getCpuUsage = (req, res) => {};
-module.exports.getStorageUsage = (req, res) => {};
+module.exports.getStorageUsage = (req, res) => {
+  try {
+    ret = child_process
+      .execSync(
+        "ssh " + edge + "  df /dev/mmcblk1p1 | tail -n 1 | awk '{print ($3)}'"
+      )
+      .toString();
+    return parseInt(ret);
+  } catch (error) {
+    console.log(error);
+    return 0;
+  }
+};
--- utils/secure_storage.js
+++ utils/secure_storage.js
@@ -20,36 +20,6 @@ exports.decipher_file_AES256 = (input_file, output_file, key) => {
   fs.writeFileSync(output_file, decrypted);
 };
 
-exports.get_cpu_usage = (edge) => {
-  child_process.exec(
-    "ssh " + edge + " vmstat 1 2 | awk 'NR==4 {print ($13+$14)}'",
-    (error, stdout, stderr) => {
-      if (error) {
-        console.log(`error: ${error.message}`);
-        return null;
-      }
-      if (stderr) {
-        return null;
-      }
-      return stdout.substring(0, 3);
-    }
-  );
-};
-
-exports.get_available_storage = (edge) => {
-  try {
-    ret = child_process
-      .execSync(
-        "ssh " + edge + "  df /dev/mmcblk1p1 | tail -n 1 | awk '{print ($3)}'"
-      )
-      .toString();
-    return parseInt(ret);
-  } catch (error) {
-    console.log(error);
-    return 0;
-  }
-};
-
 exports.random_optimization = (chunks, edges) => {
   strategy = [];
 
