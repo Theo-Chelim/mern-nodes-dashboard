e4054fd5c2cc328e6529f2c99335008e53eebe22
Théo CHELIM <theochelim@gmail.com>
2021-08-16 23:40:21 +0200

    Add edge model

--- controllers/edge.controller.js
+++ controllers/edge.controller.js
@@ -2,8 +2,22 @@ const child_process = require("child_process");
 
 const edgeUtils = require("../utils/edge.utils");
 
-module.exports.allEdges = (req, res) => {};
-module.exports.updateConfig = (req, res) => {};
+const EdgeModel = require("../models/edge.model");
+
+module.exports.allEdges = async (req, res) => {
+  await EdgeModel.find((err, docs) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json(err);
+    } else res.status(200).json(docs);
+  });
+};
+
+module.exports.updateConfig = (req, res) => {
+  console.log(req.body);
+  const example = []
+  res.status(200).json({});
+};
 
 module.exports.start = (req, res) => {};
 module.exports.stop = (req, res) => {};
@@ -12,9 +26,6 @@ module.exports.stress = (req, res) => {};
 
 module.exports.getAvailability = async (req, res) => {
   var host = "172.16.100." + req.params.id;
-  if (req.params.id == 6) {
-    host = "127.0.0.1";
-  }
   const available = await edgeUtils.get_availability(host);
   res.status(200).json({ available });
 };
