e35e5dde9bfaf4d1f71587dae0addb2353cab214
Théo CHELIM <theochelim@gmail.com>
2021-09-26 23:07:05 +0200

    Fix an other typo

--- utils/edge.utils.js
+++ utils/edge.utils.js
@@ -19,7 +19,7 @@ exports.get_availability = async (edge) => {
 
 exports.get_cpu_usage = (edge) => {
   const host = exports.get_host(edge);
-  // TODO: Test SSH commands on edges virtual
+  // TODO: Test SSH commands on virtual edges 
   child_process.exec(
     "ssh " + host + " vmstat 1 2 | awk 'NR==4 {print ($13+$14)}'",
     (error, stdout, stderr) => {
